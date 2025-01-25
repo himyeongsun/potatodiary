77f6b5d1c7bd15974b4fa653df93d25a6b579273
myeongsun <himyeongsun@gmail.com>
2025-01-25 20:02:46 +0900

    초기 세팅

---
+++ src/test/java/com/myeongsun/potatodiary/PotatodiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myeongsun.potatodiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatodiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
